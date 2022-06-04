4c0117305b4bd1631260d07fb0e849c8b58c9688
Marymezh <maria.mezhova@yahoo.com>
2022-06-04 14:29:54 +0700

    Initial Commit

---
+++ Images_Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Images_Collection
+//
+//  Created by Мария Межова on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
